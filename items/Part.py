8daa01d803e6a87a1e8d8ec1b70a2524c90f3e55
HazelCullom <hazelcullom@gmail.com>
2025-02-24 19:43:26 -0500

    part definition

--- items/Part.py
+++ items/Part.py
@@ -1,10 +1,15 @@
-from items.Weapon import Weapon
-
+from operator import add as ADD
+from operator import sub as SUB
+from operator import mul as MUL
+from operator import truediv as DIV
 
 
 class Part():
 
     # Lookup table for all parts and stat changes. Grips will have matching bonuses which need to be applied
+
+    # Grip Stats = {"Bandit": {"Mag Size": (MUL, 1.35), "Reload Time": (MUL, 1.1), "Weapon Spread": (MUL,1.15)}, ...}
+    # Grip Match Bonus = {"Bandit": {"Mag Size": (ADD,4), "Reload Time": (DIV,1.3)}, ...}
 
     def __init__(self):
         # instance variables
