621225287cce40e35a4880f04b27db0e164ac82e
HazelCullom <hazelcullom@gmail.com>
2025-02-24 19:43:34 -0500

    more part

--- items/Part.py
+++ items/Part.py
@@ -6,10 +6,11 @@ from operator import truediv as DIV
 
 class Part():
 
-    # Lookup table for all parts and stat changes. Grips will have matching bonuses which need to be applied
+    # Lookup table for all parts and stat changes. Grips will have matching bonuses which need to be applied.
+    # should put in another file and import 
 
-    # Grip Stats = {"Bandit": {"Mag Size": (MUL, 1.35), "Reload Time": (MUL, 1.1), "Weapon Spread": (MUL,1.15)}, ...}
-    # Grip Match Bonus = {"Bandit": {"Mag Size": (ADD,4), "Reload Time": (DIV,1.3)}, ...}
+    # Pistol Grip Stats = {"Bandit": {"Mag Size": (MUL, 1.35), "Reload Time": (MUL, 1.1), "Weapon Spread": (MUL,1.15)}, "Dahl": {"Accuracy Regen": (MUL, 1.1), "Impulse Accuracy": (DIV, 1.15), "Max Accuracy": (MUL, 1.15), "Weapon Damage": (DIV, 1.06)}}
+    # Pistol Grip Match Bonus = {"Bandit": {"Mag Size": (ADD,4), "Reload Time": (DIV, 1.3)}, "Dahl", {"Mag Size": (ADD, 3), "Reload Time": (DIV, 1.3)}}
 
     def __init__(self):
         # instance variables
